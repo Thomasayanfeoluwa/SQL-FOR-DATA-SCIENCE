c202e001b797199cfa88228e81122749ebf2af08
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-10 15:01:58 +0100

    distinct

--- Postgresql Queries/ORDER BY,LIMIT,DISTINCT,AND RENAME COLUMNS.sql
+++ Postgresql Queries/ORDER BY,LIMIT,DISTINCT,AND RENAME COLUMNS.sql
@@ -5,3 +5,18 @@ ORDER BY employee_id DESC
 SELECT *
 FROM employees
 ORDER BY department 
+
+SELECT *
+FROM employees
+ORDER BY salary DESC
+
+-- Return the distinct departments of employees in the employees table, ordered alphabetically.
+SELECT DISTINCT department
+FROM employees
+ORDER BY department
+
+-- Return the distinct departments of employees in the employees table, ordered alphabetically, and limit the results to 5 rows.
+SELECT DISTINCT department
+FROM employees
+ORDER BY department
+LIMIT 5
