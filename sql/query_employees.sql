fb5c6d0870017d849e63d3c13145d876fe5c7688
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-10 13:41:14 +0100

    AND/OR

--- sql/query_employees.sql
+++ sql/query_employees.sql
@@ -1,3 +1,34 @@
 SELECT *
 FROM employees
-WHERE department = 'Tools'
+WHERE 
+    department = 'Tools'
+    AND region_id = 3
+    -- AND salary > 50000
+    OR salary > 100000
+    
+
+SELECT * 
+FROM employees
+WHERE
+    department = 'Beauty'
+    AND(region_id = 3
+    OR salary > 50000)
+
+
+SELECT *
+FROM employees
+WHERE
+    department IN ('Sports', 'Clothing')
+
+
+SELECT *
+FROM employees
+WHERE
+    department NOT IN ('Sports', 'Clothing')
+
+
+
+SELECT *
+FROM employees
+WHERE
+    department <> 'Movies'
