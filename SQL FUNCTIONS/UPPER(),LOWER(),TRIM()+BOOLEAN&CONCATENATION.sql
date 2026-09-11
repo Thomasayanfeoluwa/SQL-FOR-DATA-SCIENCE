19df7090c6c97a036a9d927d4eb4b0ceb20f8b22
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 10:20:02 +0100

    checkpoint(project): update UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION

--- SQL FUNCTIONS/UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION.sql
+++ SQL FUNCTIONS/UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION.sql
@@ -1,2 +1,14 @@
-SELECT UPPER(firt_name), LOWER(last_name)
+SELECT UPPER(first_name), LOWER(last_name)
 FROM employees
+
+SELECT LENGTH(first_name), LOWER(last_name)
+FROM employees
+
+SELECT ('  HELLO THERE  ')
+
+SELECT TRIM('  HELLO THERE  ')
+
+SELECT LENGTH(TRIM('  HELLO THERE  '))
+
+
+SELECT
