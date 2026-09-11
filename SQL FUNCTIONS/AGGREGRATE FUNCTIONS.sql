eb1b9b8d812458ec2badc8a1a4a18fc6932db6e8
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 12:40:02 +0100

    checkpoint(project): update AGGREGRATE FUNCTIONS

---
+++ SQL FUNCTIONS/AGGREGRATE FUNCTIONS.sql
@@ -0,0 +1,16 @@
+SELECT MAX(salary)
+FROM employees
+
+SELECT MIN(salary)
+FROM employees
+
+SELECT AVG(salary)
+FROM employees
+
+SELECT ROUND(AVG(salary))
+FROM employees
+
+SELECT ROUND(AVG(salary), 2)
+FROM employees
+
+
