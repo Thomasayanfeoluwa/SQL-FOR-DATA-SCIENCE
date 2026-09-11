7451a2eced753b3c91681abf15a0fc0b4d523cb8
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 21:50:02 +0100

    checkpoint(project): update GROUP BY

--- CLAUSE/GROUP BY.sql
+++ CLAUSE/GROUP BY.sql
@@ -68,4 +68,4 @@ ORDER BY COUNT(*) DESC
 SELECT gender, region_id, MIN(salary) AS min_salary, 
     MAX(salary) AS max_salry, ROUND(AVG(salary) AS avg_salary)
 FROM employees
-ORDER BY gender
+ORDER BY gender, region_id
