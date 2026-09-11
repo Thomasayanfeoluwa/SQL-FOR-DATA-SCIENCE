745307a170744c08cd250b2e3c9e16fa57ba52aa
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 11:50:02 +0100

    checkpoint(project): update SUBSTRING(),REPLACE(),POSITION&COALESCE

--- SQL FUNCTIONS/SUBSTRING(),REPLACE(),POSITION&COALESCE.sql
+++ SQL FUNCTIONS/SUBSTRING(),REPLACE(),POSITION&COALESCE.sql
@@ -4,18 +4,27 @@ SELECT SUBSTRING('This is test data' FROM 6 FOR 7) AS "Substring Example";
 SELECT SUBSTRING('This is test data' FROM 6) AS "Substring Example";
 
 
--- This SQL script demonstrates the use of SUBSTRING(), REPLACE(), POSITION(), and COALESCE() functions.
+""" This SQL script demonstrates the use of SUBSTRING(), REPLACE(), POSITION(), and COALESCE() functions. """
+
+-- QUERY 1: Using SUBSTRING() to extract a portion of a string
 SELECT department,
-    SUBSTRING(department, 1, 3) AS 'First 3 Letters'
+    SUBSTRING(department, 1, 3) AS "First 3 Letters"
 FROM employees
 
-
+-- QUERY 2: Using REPLACE() to replace a substring within a string
 SELECT department, 
     REPLACE(department, 'Clothing', 'Apparel') AS "Replaced Department"
 FROM employees
 
+-- QUERY 3: Using POSITION() to find the position of a substring within a string
+SELECT department,
+    POSITION('o' IN department) AS "Position of o in Department"
+FROM employees
 
-
+-- QUERY 4: Using COALESCE() to handle NULL values
+SELECT department,
+    COALESCE(email, 'No Email') AS "Email"
+FROM employees
 
 
 
