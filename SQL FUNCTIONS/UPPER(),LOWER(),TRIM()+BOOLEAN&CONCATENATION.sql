d27f4f50ea39e2ecfbb9c35f458994bd30418636
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 11:00:02 +0100

    checkpoint(project): update UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION

--- SQL FUNCTIONS/UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION.sql
+++ SQL FUNCTIONS/UPPER(),LOWER(),TRIM()+BOOLEAN&CONCATENATION.sql
@@ -26,10 +26,14 @@ SELECT first_name || ' ' || last_name AS "Full Name", department, (salary > 1000
 FROM employees
 ORDER BY salary DESC
 
-
+-- Boolean expressions USING IN
 SELECT department, ('Clothing' IN (department)) AS "In Clothing Department"
 FROM employees
 
+SELECT department, (department IN ('Clothing', 'Sports')) AS "In Clothing or Sports"
+FROM employees
 
+-- Boolean expressions USING LIKE
 SELECT department, (department LIKE '%oth%') AS "Contains oth"
 FROM employees
+
