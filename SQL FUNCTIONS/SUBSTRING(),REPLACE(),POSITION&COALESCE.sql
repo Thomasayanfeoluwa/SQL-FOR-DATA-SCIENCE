210a4ee5f00b5a91bd4016308e49f175a191dd4a
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-11 11:20:02 +0100

    checkpoint(project): update SUBSTRING(),REPLACE(),POSITION&COALESCE

--- SQL FUNCTIONS/SUBSTRING(),REPLACE(),POSITION&COALESCE.sql
+++ SQL FUNCTIONS/SUBSTRING(),REPLACE(),POSITION&COALESCE.sql
@@ -1,12 +1,14 @@
-SELECT SUBSTRING('This is test data' FROM 6 FOR ) AS "Substring Example";
+-- SQL script of testing SUBSTRING()
+SELECT SUBSTRING('This is test data' FROM 6 FOR 7) AS "Substring Example";
+
+SELECT SUBSTRING('This is test data' FROM 6) AS "Substring Example";
 
 
 
 
 
 
-
-
+-- This SQL script demonstrates the use of SUBSTRING(), REPLACE(), POSITION(), and COALESCE() functions.
 SELECT SUBSTRING(first_name, 1, 3) AS "First 3 Letters", 
     REPLACE(last_name, 'a', 'A') AS "Replaced Last Name",
     POSITION('a' IN last_name) AS "Position of a in Last Name",
