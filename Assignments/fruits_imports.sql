f152ec4755fb2caaea39ed8f99e4fff0f6c3a7f0
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-14 11:20:02 +0100

    checkpoint(data): update fruits_imports

--- Assignments/fruits_imports.sql
+++ Assignments/fruits_imports.sql
@@ -1,1 +1,16 @@
-SELECT datname FROM pg_database;
+-- SELECT datname FROM pg_database;
+
+SELECT current_database(), current_schema();
+
+SHOW search_path;
+
+
+-- SELECT table_schema, table_name
+-- FROM information_schema.tables
+-- WHERE table_schema NOT IN ('pg_catalog', 'information_schema')
+-- ORDER BY table_schema, table_name;
+
+
+-- SELECT table_schema, table_name
+-- FROM information_schema.tables
+-- WHERE table_name = 'employees';
