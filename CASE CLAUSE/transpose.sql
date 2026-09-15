2356542843d1de4423383d5092ae5c701717f7f2
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 17:08:04 +0100

    checkpoint(project): update transpose

--- CASE CLAUSE/transpose.sql
+++ CASE CLAUSE/transpose.sql
@@ -3,3 +3,8 @@ SELECT SUM( CASE WHEN salary < 100000 THEN 1 ELSE 0 END) AS "UNDER PAID",
     SUM( CASE WHEN salary > 150000 THEN 1 ELSE 0 END) AS "EXECUTIVE"
 FROM employees
 
+
+
+SELECT department, COUNT(*)
+FROM employees
+WHERE de
