5d0094e4ed26ad8bcb8bc963dfc9d2ef444077eb
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 15:27:29 +0100

    checkpoint(project): update case_query

--- CASE CLAUSE/case_query.sql
+++ CASE CLAUSE/case_query.sql
@@ -17,3 +17,13 @@ CASE
 END
 FROM employees
 ORDER BY salary 
+
+
+SELECT first_name, salary,
+CASE
+    WHEN salary < 100000 THEN 'UNDER PAID'
+    WHEN salary > 100000 AND salary < 160000 THEN 'PAID WELL'
+    ELSE 'EXECUTIVES'
+END
+FROM employees
+ORDER BY salary DESC
