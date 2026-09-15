621498a5abf85d837e692ff359555d3978838512
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 19:36:02 +0100

    checkpoint(project): update query

--- UNION, UNION ALL, EXCEPT/query.sql
+++ UNION, UNION ALL, EXCEPT/query.sql
@@ -5,3 +5,13 @@ SELECT department
 FROM departments
 LIMIT 10
 
+SELECT row_number() OVER (ORDER BY department) AS row_no, department
+FROM (
+    SELECT department
+    FROM employees
+    UNION
+    SELECT department
+    FROM departments
+) AS combined
+LIMIT 10;
+
