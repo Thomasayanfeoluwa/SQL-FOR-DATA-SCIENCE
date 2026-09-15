0a06c2d1d471c53611124324cd567a7e50de6e62
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 15:18:45 +0100

    CASE

---
+++ CASE CLAUSE/case_query.sql
@@ -0,0 +1,19 @@
+SELECT first_name, salary,
+CASE
+    WHEN salary < 100000 THEN 'UNDER PAID'
+    WHEN salary > 100000 THEN 'WELL PAID'
+    ELSE 'UNPAID'
+END
+FROM employees
+ORDER BY salary DESC
+
+
+
+SELECT first_name, salary,
+CASE
+    WHEN salary < 100000 THEN 'UNDER PAID'
+    WHEN salary > 100000 THEN 'WELL PAID'
+    ELSE 'UNPAID'
+END
+FROM employees
+ORDER BY salary 
