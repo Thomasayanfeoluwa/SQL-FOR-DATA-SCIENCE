c9aabbab0ac4e2370f4894604e5595879bb68dc0
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 17:36:02 +0100

    checkpoint(project): update transpose

--- CASE CLAUSE/transpose.sql
+++ CASE CLAUSE/transpose.sql
@@ -1,18 +1,21 @@
-SELECT SUM( CASE WHEN salary < 100000 THEN 1 ELSE 0 END) AS "UNDER PAID",
-    SUM( CASE WHEN salary > 100000 AND salary < 150000 THEN 1 ELSE 0 END) AS "PAID WELL",
-    SUM( CASE WHEN salary > 150000 THEN 1 ELSE 0 END) AS "EXECUTIVE"
-FROM employees
+SELECT SUM(CASE WHEN salary < 100000 THEN 1 ELSE 0 END) AS "UNDER PAID",
+    SUM(CASE WHEN salary > 100000 AND salary < 150000 THEN 1 ELSE 0 END) AS "PAID WELL",
+    SUM(CASE WHEN salary > 150000 THEN 1 ELSE 0 END) AS "EXECUTIVE"
+FROM employees;
 
 
 
 SELECT department, COUNT(*)
 FROM employees
-WHERE department IN ('Sport', 'Tools', 'Clothing', 'Computers')
-GROUP BY department
+WHERE department IN ('Sports', 'Tools', 'Clothing', 'Computers')
+GROUP BY department;
 
 
-SELECT SUM( CASE WHEN department = 'Sport' THEN 1 ELSE 0 END),
-SELECT SUM( CASE WHEN department = 'Tool' THEN 1 ELSE 0 END),
-SELECT SUM( CASE WHEN department = 'Clothing' THEN 1 ELSE 0 END),
-SELECT SUM( CASE WHEN department = 'Computers' THEN 1 ELSE 0 END),
-FROM departments
+SELECT SUM(CASE WHEN department = 'Sports' THEN 1 ELSE 0 END) AS "Sports Employees",
+    SUM(CASE WHEN department = 'Tools' THEN 1 ELSE 0 END) AS "Tools Employees",
+    SUM(CASE WHEN department = 'Clothing' THEN 1 ELSE 0 END) AS "Clothing Employees",
+    SUM(CASE WHEN department = 'Computers' THEN 1 ELSE 0 END) AS "Computers Employees"
+FROM employees;
+
+
+
