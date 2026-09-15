3c4915a9309f6a39ef845e18f6b409f9e91c35e3
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 13:43:16 +0100

    DELETE FROM

--- SUBQUERY/duplicates.sql
+++ SUBQUERY/duplicates.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT(name)
 FROM dupes
-GROUP BY name
+GROUP BY name;
 
 SELECT *
 FROM dupes
@@ -10,4 +10,14 @@ WHERE id IN (
     GROUP BY name
 );
 
+DELETE FROM dupes
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM dupes
+    GROUP BY name
+);
 
+
+SELECT DISTINCT(name), id
+FROM dupes
+GROUP BY name, id;
