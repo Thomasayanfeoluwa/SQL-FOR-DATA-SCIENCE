f3a001b32a4e059006be330b7e4c9c98a22a4964
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-15 20:47:20 +0100

    checkpoint(project): update query

--- UNION, UNION ALL, EXCEPT/query.sql
+++ UNION, UNION ALL, EXCEPT/query.sql
@@ -1,14 +1,52 @@
+-- Active: 1788967706440@@127.0.0.1@5432@SQL FOR DATA SCIENCE
 SELECT department
 FROM employees
 UNION
 SELECT department
 FROM departments
-LIMIT 15
+LIMIT 15;
 
 SELECT department
 FROM employees
 UNION ALL
 SELECT department
 FROM departments
-LIMIT 15
+LIMIT 15;
+
+SELECT department, first_name
+FROM employees
+UNION
+SELECT department, division
+FROM departments
+ORDER BY department;
+
+SELECT department
+FROM employees
+UNION ALL
+SELECT department
+FROM departments
+UNION
+SELECT country
+FROM regions
+ORDER BY department;
+
+SELECT DISTINCT department
+FROM employees
+EXCEPT
+SELECT department
+FROM departments;
+
+SELECT department
+FROM departments
+EXCEPT
+SELECT DISTINCT department
+FROM employees
+
+SELECT department, COUNT(*)
+FROM employees
+GROUP BY department
+UNION ALL
+SELECT 'TOTAL', COUNT(*)
+FROM employees
+ORDER BY department
 
