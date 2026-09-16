eefcfa5e88a01bd101f32122c54e43c9e5048697
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-16 12:29:40 +0100

    UNION ORDER BY

--- JOINS/query.sql
+++ JOINS/query.sql
@@ -44,3 +44,42 @@ WHERE hire_date = (
     FROM employees e2
 )
 
+SELECT first_name, department, hire_date, country
+FROM employees e
+INNER JOIN regions r
+    ON e.region_id = r.region_id
+WHERE hire_date = (
+    SELECT MIN(hire_date)
+    FROM employees e2
+)
+UNION 
+SELECT first_name, department, hire_date, country
+FROM employees e
+INNER JOIN regions r
+    ON e.region_id = r.region_id
+WHERE hire_date = (
+    SELECT MAX(hire_date)
+    FROM employees e2
+)
+ORDER BY hire_date 
+
+(SELECT first_name, department, hire_date, country
+FROM employees e
+INNER JOIN regions r
+    ON e.region_id = r.region_id
+WHERE hire_date = (
+    SELECT MIN(hire_date)
+    FROM employees e2
+)
+LIMIT 1)
+UNION 
+SELECT first_name, department, hire_date, country
+FROM employees e
+INNER JOIN regions r
+    ON e.region_id = r.region_id
+WHERE hire_date = (
+    SELECT MAX(hire_date)
+    FROM employees e2
+)
+ORDER BY hire_date 
+
