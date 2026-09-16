2ead58b3bb0b00712b2888d2db606e309d15f962
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-16 14:06:14 +0100

    row_number over

--- WINDOWS FUNCTIONS/OVER.sql
+++ WINDOWS FUNCTIONS/OVER.sql
@@ -22,7 +22,13 @@ SUM(salary) OVER()
 FROM employees
 
 SELECT first_name, department,
-COUNT(*) OVER(PARTITION BY department) dept_count,
+COUNT(*) OVER(PARTITION BY department) dept_count, region_id,
 COUNT(*) OVER(PARTITION BY region_id) reg_count
 FROM employees
+
+SELECT ROW_NUMBER() OVER() AS row_number,
+    first_name, department, region_id,
+    COUNT(*) OVER() AS total_rows
+FROM employees
+WHERE region_id = 3
 
