1f973e8289e7427b62e7c893bfecdfb22e7db90d
Adegoke Idowu Thomas <ayanfeoluwadegoke@gmail.com>
2026-09-16 13:23:02 +0100

    viewing table

---
+++ VIEWS/views.sql
@@ -0,0 +1,16 @@
+CREATE VIEW v_employee_information AS
+SELECT first_name, email, e.department, salary, division, region, country
+FROM employees e, departments d, regions r
+WHERE e.department = d.department
+    AND e.region_id = r.region_id
+
+
+SELECT *
+FROM v_employee_information
+
+SELECT *
+FROM departments
+
+SELECT *
+FROM regions
+
